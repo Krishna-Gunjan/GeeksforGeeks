6dd71547592a5cb34cb09439e78adea787905861
Krishna Gunjan <146088165+Krishna-Gunjan@users.noreply.github.com>
2024-06-09 22:51:26 +0530

    Problem of the Day: 9 June 2024

---
+++ 9 June 2024/sol.py
@@ -0,0 +1,11 @@
+QUESTION:
+
+Convert array into Zig-Zag fashion
+
+Given an array `arr` of distinct elements of size n,
+the task is to rearrange the elements of the array in a zig-zag fashion,
+so that the converted array should be in the below form: 
+
+arr[0] < arr[1]  > arr[2] < arr[3] > arr[4] < . . . . arr[n-2] < arr[n-1] > arr[n]
+
+Note: Modify the given arr[] only, If your transformation is correct, the output will be 1 else the output will be 0.
