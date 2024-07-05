a851ea75e0f1923f527eca75a42702bc6c2d775c
Krishna Gunjan <146088165+Krishna-Gunjan@users.noreply.github.com>
2024-07-05 19:38:19 +0530

    Problem of the Day: 5 July 2024

--- 5 July 2024/sol.py
+++ 5 July 2024/sol.py
@@ -1,24 +1,24 @@
 class Solution:
-
-  def verticalWidth(self, root):
     
-    if not root:
-        return 0
-    
-    min_max = [0, 0]
-    
-    minMax(root, 0, min_max)
-    
-    return min_max[1] - min_max[0] + 1
-    
-def minMax(self, root, depth, min_max):
-    if not root:
-        return
-    
-    if depth < min_max[0]:
-        min_max[0] = depth
-    if depth > min_max[1]:
-        min_max[1] = depth
+    def verticalWidth(root):
         
-    minMax(root.left, depth - 1, min_max)
-    minMax(root.right, depth + 1, min_max)
+        if not root:
+            return 0
+        
+        min_max = [0, 0]
+        
+        minMax(root, 0, min_max)
+        
+        return min_max[1] - min_max[0] + 1
+        
+    def minMax(root, depth, min_max):
+        if not root:
+            return
+        
+        if depth < min_max[0]:
+            min_max[0] = depth
+        if depth > min_max[1]:
+            min_max[1] = depth
+            
+        minMax(root.left, depth - 1, min_max)
+        minMax(root.right, depth + 1, min_max)
